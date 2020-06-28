5ec648ed17de3158e4acb2591e4db6331a500e7a
AashishNagpal13 <32671159+AashishNagpal13@users.noreply.github.com>
2020-06-28 15:29:06 +0530

    changes for basic chart

--- src/App.js
+++ src/App.js
@@ -1,57 +1,106 @@
-import React, { Component } from 'react';
+import React, {Component}  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './components/chart'
 
-import Chart from 'react-c3-component';
-import 'c3/c3.css';
 
-class Chart extends React.Component {
-  constructor(props) {
-    super(props);
+// import Chart from 'react-c3-component';
+// import 'c3/c3.css';
 
-    this.state = {
-      column1: ['data1', 30, 200, 100, 400, 150, 250],
-      column2: ['data2', 50, 20, 10, 40, 15, 25],
-    };
+// class Chart extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.changeData = this.changeData.bind(this);
-  }
+//     this.state = {
+//       column1: ['data1', 30, 200, 100, 400, 150, 250],
+//       column2: ['data2', 50, 20, 10, 40, 15, 25],
+//     };
 
-  renderChart() {
-    c3.generate({
-      bindto: "#chart1",
-      data: {
-        columns: [this.state.column1, this.state.column2],
-      },
-    });
-  }
+//     this.changeData = this.changeData.bind(this);
+//   }
 
-  componentDidMount() {
-    this.renderChart();
-  }
+//   renderChart() {
+//     c3.generate({
+//       bindto: "#chart1",
+//       data: {
+//         columns: [this.state.column1, this.state.column2],
+//       },
+//     });
+//   }
 
-  componentDidUpdate() {
-    this.renderChart();
-  }
+//   componentDidMount() {
+//     this.renderChart();
+//   }
 
-  // Changes data to something arbitrary on button click
-  changeData() {
-    this.setState({
-      column1: ['data1', 70, 120, 30, 300, 230, 300],
-      column2: ['data2', 100, 120, 50, 140, 150, 80],
-    });
-  }
+//   componentDidUpdate() {
+//     this.renderChart();
+//   }
 
+//   // Changes data to something arbitrary on button click
+//   changeData() {
+//     this.setState({
+//       column1: ['data1', 70, 120, 30, 300, 230, 300],
+//       column2: ['data2', 100, 120, 50, 140, 150, 80],
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div id="chart1"> 
+//         </div>
+        
+//         <button onClick={this.changeData}>Change< /button>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
   render() {
     return (
-      <div>
-        <div id="chart1"> 
-        </div>
-        
-        <button onClick={this.changeData}>Change< /button>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Chart/>
+
       </div>
     );
   }
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
